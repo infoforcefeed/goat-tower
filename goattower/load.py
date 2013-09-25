ef8fa8335320a9a83d0b54ae50a58f335b63ce5e
Nick Tzaperas <nick@nijotz.com>
2013-09-24 18:37:54 -0700

    Support loading players from fixtures

--- goattower/load.py
+++ goattower/load.py
@@ -8,28 +8,34 @@ def load(data):
     from models import engine
     Session = sessionmaker(bind=engine)
     session = Session()
+
     for actor in data:
         new_actor = models.Actor(name=actor['name'])
         new_actor.id = actor['id']
-        for command in actor['commands']:
+        new_actor.is_player = actor.get('is_player', False)
+        new_actor.parent_id = actor.get('parent_id', None)
+        session.add(new_actor)
+
+        for command in actor.get('commands', []):
             commands = []
             for regex in command['regex']:
                 new_command = models.Command(
                     regex=regex,
                     actor_id=new_actor.id)
                 commands.append(new_command)
+                session.add(new_command)
 
             code = []
             for command in command['code']:
                 method, args = command.popitem()
-                new_code = models.Code(method, args)
+                new_code = models.Code(method, json.dumps(args))
                 code.append(new_code)
+                session.add(new_code)
             
             for command in commands:
                 for cd in code:
                     command.code.append(cd)
 
-        session.add(new_actor)
     session.commit()
 
 def load_json(json_str):
