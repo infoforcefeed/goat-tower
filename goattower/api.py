25775957649519e9c2debcdd293522dd7553b270
Nick Tzaperas <nick@nijotz.com>
2013-09-25 15:22:01 -0700

    Added a couple test methods to the API

--- goattower/api.py
+++ goattower/api.py
@@ -8,6 +8,11 @@ def run_method(actor_id, method_name, args_string):
 
 
 class API(object):
-    pass
+
+    def send_text(self, actor_id, text):
+        print 'send_text: {} - {}'.format(actor_id, text)
+
+    def update_location(self, actor_id, location_id):
+        print 'update_location: {} - {}'.format(actor_id, location_id)
 
 api = API()
