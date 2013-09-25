ceaa80e82f90243244bb16119af80f0d60724eb5
Nick Tzaperas <nick@nijotz.com>
2013-09-24 19:16:50 -0700

    Started command flow through objects

--- goattower/engine.py
+++ goattower/engine.py
@@ -1,25 +1,47 @@
 import api
 import re
-from sqlalchemy.orm import sessionmaker
-from models import engine, Base, Code, Command
+from sqlalchemy.orm import aliased, sessionmaker
+from models import engine, Actor, Code, Command
 
+# start session
+Session = sessionmaker(bind=engine)
+session = Session()
 
 def handle_text(actor_id, text):   
-    # start session
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
-    # look for commands on current character
-    commands = session.query(Command).filter(Command.actor_id == actor_id).all()
     # TODO: handle multiple matches
+    # TODO: remove copypasta
+
+    # Look for commands on current character
+    commands = session.query(Command).filter(Command.actor_id == actor_id).all()
     for command in commands:
         regex = re.compile(command.regex)
         match = regex.match(text)
-        if regex.match:
+        if match:
             run_code(actor_id, command.id)
 
-    # TODO: go past character
+    # Children of character
+    commands = session.query(Actor, Command).\
+        filter(Actor.id == actor_id).\
+        join(Actor.parent, aliased=True).\
+        filter(Command.actor_id == Actor.id).all()
+    for command in commands:
+        regex = re.compile(command.regex)
+        match = regex.match(text)
+        if match:
+            run_code(actor_id, command.id)
 
+    # Location
+    location = aliased(Actor)
+    commands = session.query(Command).\
+        join(location).\
+        join(Actor, location.id == Actor.parent_id).\
+        filter(Actor.id == actor_id).all()
+    for command in commands:
+        regex = re.compile(command.regex)
+        match = regex.match(text)
+        if match:
+            run_code(actor_id, command.id)
 
 def run_code(actor_id, command_id):
     codes = session.query(Code).filter(Command.id == command_id).all()
