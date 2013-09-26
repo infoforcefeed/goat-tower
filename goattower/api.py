033441b8e44fe10f50e1ea4fb1f3ee9b9f1e5ccf
Nick Tzaperas <nick@nijotz.com>
2013-09-26 14:52:01 -0700

    API does real things other than 'print'

--- goattower/api.py
+++ goattower/api.py
@@ -1,4 +1,6 @@
 import json
+from sqlalchemy.orm import sessionmaker
+from models import engine, Actor, PlayerText
 
 
 def run_method(actor_id, method_name, args_string):
@@ -9,10 +11,20 @@ def run_method(actor_id, method_name, args_string):
 
 class API(object):
 
+    def __init__(self):
+        # start session
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+
     def send_text(self, actor_id, text):
-        print 'send_text: {} - {}'.format(actor_id, text)
+        actor = self.session.query(Actor).get(actor_id)
+        if actor.is_player:
+            self.session.add(PlayerText(actor_id, text))
+            self.session.commit()
 
     def update_location(self, actor_id, location_id):
-        print 'update_location: {} - {}'.format(actor_id, location_id)
+        actor = self.session.query(Actor).get(actor_id)
+        actor.parent_id = location_id
+        self.session.commit()
 
 api = API()
