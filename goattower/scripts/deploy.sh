6673f6aba1a283a16f93c1c9944ee3ff95647807
Kyle Terry <kyle@kyleterry.com>
2013-10-06 01:05:44 -0700

    You die after trying to fight with a goat from the goat tower.

--- goattower/scripts/deploy.sh
+++ goattower/scripts/deploy.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-time dropdb -h localhost goattower && createdb -h localhost goattower && python ../load.py ../objects/test_location.yaml yaml || python ../load.py ../objects/test_location.yaml yaml && afplay ./dogbark4.wav
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ "$(uname)" == 'Linux' ]; then
+    COMMAND=aplay
+elif [ "$(uname)" == 'Darwin' ]; then
+    COMMAND=afplay
+fi
+time dropdb -h localhost goattower && createdb -h localhost goattower && python ${BASEDIR}/../load.py ${BASEDIR}/../objects/test_location.yaml yaml || python ${BASEDIR}/../load.py ${BASEDIR}/../objects/test_location.yaml yaml && ${COMMAND} ${BASEDIR}/dogbark4.wav
